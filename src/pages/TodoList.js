a795b32faf83d52b5d4bece1f7178aa37e82b6fb
Sompasong <vongthavone@gmail.com>
2024-06-21 09:22:32 +0700

    Add comments

--- src/pages/TodoList.js
+++ src/pages/TodoList.js
@@ -14,47 +14,71 @@ import {
 
 const { Text } = Typography;
 
+//ປະກາດ TodoList compoment
 const TodoList = () => {
   const [task, setTask] = useState("");
   const [todos, setTodos] = useState([]);
   const [doneTodos, setDoneTodos] = useState([]);
 
   useEffect(() => {
+    //ຂະບວນການດຶງຂໍ້ມູນມາເກັບໄວ້ໃນ todos ແລະ doneTodos
+
+    //ຄິວຣີ້ຂໍ້ມູນຈາກຈາກ todo colecction ລຽງຕາມວັນທີທີ່ສ້າງ
     const q = query(collection(db, "todo"), orderBy("created", "desc"));
+
+    //ດຶງຂໍ້ມູນທັງໝົດມາໄວ້ໃນຮູບແບບຂອງ array
     const unsubscribe = onSnapshot(q, (snapshot) => {
+      //ອາເຣລາຍການ todo
       const todoData = [];
+
+      //ອາເຣລາຍການ doneTodo
       const doneTodoData = [];
+
+      //Loop ຂໍ້ມູນຈາກ snapshot
       snapshot.forEach((doc) => {
         const data = { ...doc.data(), id: doc.id };
+
+        //ກໍລະນີ checked ມີຄ່າເປັນ true ໃຫ້ເພີ່ມເຂົ້າໃນ doneTodo
         if (data.checked) {
           doneTodoData.push(data);
         } else {
+          //ກໍລະນີເປັນ false ໃຫ້ເພີ່່ມເຂົ້າໃນ todo
           todoData.push(data);
         }
       });
+
+      //setState ຂອງທັງ todos ແລະ doneTodos
       setTodos(todoData);
       setDoneTodos(doneTodoData);
     });
 
+    //ທຳການສົ່ງຄ່າກັບເພື່ອໃຊ້ງານ
     return () => unsubscribe();
   }, []);
 
+  //ຟັງຊັ້ນສຳລັບເພີ່ມຂໍ້ມູນ
   const handleAddTask = async () => {
+    //trim ເພື່ອລົບຍະວ່າງທີ່ບໍ່ຈຳເປັນອອກ
     if (task.trim()) {
+      //ເພີ່ມເຂົ້າໃນ todo collection
       await addDoc(collection(db, "todo"), {
         task: task.trim(),
         created: serverTimestamp(),
         checked: false,
       });
+
+      //ກຳນົດໃຫ້ເປັນຄ່າວ່າງ
       setTask("");
     }
   };
 
+  //ຟັງຊັ້ນໝາຍຕິກຖືກ
   const handleCheck = async (id, checked) => {
     const todoDoc = doc(db, "todo", id);
     await updateDoc(todoDoc, { checked });
   };
 
+  //ຟັງຊັ້ນ enter event
   const handleKeyDown = (e) => {
     if (e.key === "Enter") {
       handleAddTask();
